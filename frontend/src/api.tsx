b2d4cea2a2327e4d83b6b17eabfa334497b3e67a
Nicklas Gummesson <nicklas.gummesson@gmail.com>
2023-07-13 15:58:53 +0200

    Make simulation work
    - DB connectivity
    - Env var usages

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,8 +1,9 @@
-const baseUrl: string =
-  process.env.REACT_APP_ENV === 'dev'
-    ? 'http://localhost:8080'
-    : 'https://rides.jurajmajerik.com';
+// const baseUrl: string =
+//   process.env.REACT_APP_ENV === 'dev'
+//     ? 'http://localhost:8080'
+//     : 'https://rides.jurajmajerik.com';
 
+const baseUrl: string = '';
 export const api: { [key: string]: any } = {};
 
 api.get = async (endpoint: string): Promise<any> => {
